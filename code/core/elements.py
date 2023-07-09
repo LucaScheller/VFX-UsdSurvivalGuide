60f8b47b0eb61bc5007f556e2702e8064facf4c2
Luca Scheller <luca.scheller@gmail.com>
2023-07-09 23:08:53 +0200

    Finish path section

--- code/core/elements.py
+++ code/core/elements.py
@@ -7,13 +7,51 @@ def example():
 """
 
 """Path.md"""
+
+#// ANCHOR: pathSummary
+from pxr import Sdf
+# Prims
+prim_path = Sdf.Path("/set/bicycle")
+prim_path_str = Sdf.Path("/set/bicycle").pathString # Returns the Python str "/set/bicycle"
+# Properties (Attribute/Relationship)
+property_path = Sdf.Path("/set/bicycle.size")
+property_with_namespace_path = Sdf.Path("/set/bicycle.tire:size")
+# Relationship targets
+prim_rel_target_path = pxr.Sdf.Path("/set.bikes[/set/bicycles]")           # Prim to prim linking (E.g. path collections)
+attribute_rel_target_path = pxr.Sdf.Path("/set.bikes[/set/bicycles].size") # Attribute to attribute linking (E.g. serializing node graph connections to Usd)
+#// ANCHOR_END: pathSummary
+
 #// ANCHOR: pathBasics
 from pxr import Sdf
 path = Sdf.Path("/set/bicycle")
 path_name = path.name     # Similar to os.path.basename(), returns the last element of the path
 path_empty = path.isEmpty # Property to check if path is empty
-path_startswith = path.HasPrefix(Sdf.Path("/set")) # Same as Python Str's .startswith
-path = Sdf.Path(Sdf.childDelimiter.join(["set", "bicycle"])) # Manually join individual path elements
+# Path validation (E.g. for user created paths)
+Sdf.Path.IsValidPathString("/some/_wrong!_/path") # Returns: False, 'Error Message'
+# Joint paths (Similar to os.path.join())
+path = Sdf.Path("/set/bicycle")
+path.AppendPath(Sdf.Path("frame/screws")) # Returns: Sdf.Path("/set/bicycle/frame/screws")
+# Manually join individual path elements
+path = Sdf.Path(Sdf.childDelimiter.join(["set", "bicycle"])) 
+# Get the parent path
+parent_path = path.GetParentPath() # Returns Sdf.Path("/set")
+parent_path.IsRootPrimPath()       # Returns: True (Root prims are prims that only
+                                   #                have a single '/')        
+ancestor_range = path.GetAncestorsRange() # Returns an iterator for the parent paths, the same as recursively calling GetParentPath()
+# Add child path
+child_path = path.AppendChild("wheel") # Returns: Sdf.Path("/set/bicycle/wheel")
+# Check if path is a prim path (and not a attribute/relationship path)
+path.IsPrimPath() # Returns: True
+# Check if path starts with another path
+# Important: It actually compares individual path elements (So it is not a str.startswith())
+Sdf.Path("/set/cityA/bicycle").HasPrefix(Sdf.Path("/set"))      # Returns: True
+Sdf.Path("/set/cityA/bicycle").HasPrefix(Sdf.Path("/set/city")) # Returns: False
+Sdf.Path("/set/bicycle").GetCommonPrefix(Sdf.Path("/set"))      # Returns: Sdf.Path("/set")
+# Relative/Absolute paths
+path = Sdf.Path("/set/cityA/bicycle")
+rel_path = path.MakeRelativePath("/set")     # Returns: Sdf.Path('cityA/bicycle')
+abs_path = rel_path.MakeAbsolutePath("/set") # Returns: Sdf.Path('/set/cityA/bicycle')
+abs_path.IsAbsolutePath()                    # Returns: True -> Checks path[0] == "/"
 # Do not use this is performance critical loops
 # See for more info: https://openusd.org/release/api/_usd__page__best_practices.html
 # This gives you a standard python string
@@ -24,6 +62,7 @@ path_str = path.pathString
 from pxr import Sdf
 # Shortcut for Sdf.Path("/")
 root_path = Sdf.Path.absoluteRootPath
+root_path == Sdf.Path("/") # Returns: True
 # We'll cover in a later section how to rename/remove things in Usd,
 # so don't worry about the details how this works yet. Just remember that
 # an emptyPath exists and that its usage is to remove something.
@@ -38,89 +77,57 @@ edit.Add(src_path, dst_path)
 # in the path via the "." (Sdf.Path.propertyDelimiter) token
 from pxr import Sdf
 path = Sdf.Path("/set/bicycle.size")
-property_name = path.name # Be aware, this will return "size" (last element)
+property_name = path.name # Be aware, this will return 'size' (last element)
+# Append property to prim path
+Sdf.Path("/set/bicycle").AppendProperty("size") # Returns: Sdf.Path("/set/bicycle.size")
 # Properties can also be namespaced with ":" (Sdf.Path.namespaceDelimiter)
-path = Sdf.Path("/set/bicycle.tire:size")
+path = Sdf.Path("/set/bicycle.tire:size").name 
+property_name = path.name                 # Returns: 'tire:size'
+property_name = path.ReplaceName("color") # Returns: Sdf.Path("/set/bicycle.color")
+# Check if path is a property path (and not a prim path)
+path.IsPropertyPath() # Returns: True
+# Check if path is a property path (and not a prim path)
+Sdf.Path("/set/bicycle.tire:size").IsPrimPropertyPath()  # Returns: True
+Sdf.Path("/set/bicycle").IsPrimPropertyPath()            # Returns: False
 # Convenience methods
 path = Sdf.Path("/set/bicycle").AppendProperty(Sdf.Path.JoinIdentifier(["tire", "size"]))
-namespaced_elements = Sdf.Path.TokenizeIdentifier("tire:size") # Returns: ["tire", "size"]
+namespaced_elements = Sdf.Path.TokenizeIdentifier("tire:size")   # Returns: ["tire", "size"]
+last_element = Sdf.path.StripNamespace("/set/bicycle.tire:size") # Returns: 'size'
+# With GetPrimPath we can strip away all property encodings
+path = Sdf.Path("/set/bicycle.tire:size")
+prim_path = path.GetPrimPath(path) # Returns: Sdf.Path('/set/bicycle')
+
+# We can't actually differentiate between a attribute and relationship based on the property path.
+# Hence the "Property" terminology.
+# In practise we rarely use/see this as this is a pretty low level API usecase.
+# The only 'common' case, where you will see this is when calling the Sdf.Layer.Traverse function.
+# To encode prim relation targets, we can use:
+prim_rel_target_path = Sdf.Path("/set.bikes[/set/bicycle]")
+prim_rel_target_path.IsTargetPath() # Returns: True
+prim_rel_target_path = Sdf.Path("/set.bikes").AppendTarget("/set/bicycle")
+# We can also encode attribute relation targets (For example shader node graph connections):
+attribute_rel_target_path = pxr.Sdf.Path("/set.bikes[/set/bicycles].size")
+attribute_rel_target_path.IsRelationalAttributePath()  # Returns: True
 #// ANCHOR_END: pathProperties
 
+#// ANCHOR: pathVariants
+# Variants (see the next sections) are also encoded
+# in the path via the "{variantSetName=variantName}" syntax.
+from pxr import Sdf
+path = Sdf.Path("/set/bicycle")
+variant_path = path.AppendVariantSelection("style", "blue") # Returns: Sdf.Path('/set/bicycle{style=blue}')
+variant_path = Sdf.Path('/set/bicycle{style=blue}frame/screws')
+# Property path to prim path with variants
+property_path = Sdf.Path('/set/bicycle{style=blue}frame/screws.size')
+variant_path = property_path.GetPrimOrPrimVariantSelectionPath() # Returns: Sdf.Path('/set/bicycle{style=blue}frame/screws')
+# Typical iteration example:
+variant_path = Sdf.Path('/set/bicycle{style=blue}frame/screws')
+if variant_path.ContainsPrimVariantSelection():          # Returns: True # For any variant selection in the whole path
+    for parent_path in variant_path.GetAncestorRange():
+        if parent_path.IsPrimVariantSelectionPath():
+            print(parent_path.GetVariantSelection())     # Returns: ('style', 'blue')
 
-
-
-Sdf.Path.emptyPath
-Sdf.Path.propertyDelimiter
-Sdf.Path.namespaceDelimiter
-Sdf.Path.childDelimiter
-Sdf.Path.absoluteIndicator
-Sdf.Path.parentPathElement
-
-
-'AncestorsRange',
-'AppendChild', 
-'AppendElementString',
-'AppendExpression', 
-'AppendMapper', 
-'AppendMapperArg', 
-'AppendPath', 
-'AppendProperty',
-'AppendRelationalAttribute', 
-'AppendTarget', 
-'AppendVariantSelection', 
-'ContainsPrimVariantSelection',
-'ContainsPropertyElements',
-'ContainsTargetPath',
-'FindLongestPrefix', 
-'FindLongestStrictPrefix', 
-'FindPrefixedRange', 
-'GetAbsoluteRootOrPrimPath', 
-'GetAllTargetPathsRecursively',
-'GetAncestorsRange', 
-'GetCommonPrefix', 
-'GetConciseRelativePaths', 
-'GetParentPath',
-'GetPrefixes',
-'GetPrimOrPrimVariantSelectionPath',
-'GetPrimPath',
-'GetVariantSelection',
-'HasPrefix',
-'IsAbsolutePath',
-'IsAbsoluteRootOrPrimPath', 
-'IsAbsoluteRootPath', 
-'IsExpressionPath', 
-'IsMapperArgPath',
-'IsMapperPath',
-'IsNamespacedPropertyPath', 
-'IsPrimPath', 
-'IsPrimPropertyPath', 
-'IsPrimVariantSelectionPath', 
-'IsPropertyPath',
-IsRelationalAttributePath',
-'IsRootPrimPath',
-'IsTargetPath',
-'IsValidIdentifier',
-'IsValidNamespacedIdentifier',
-'IsValidPathString', 
-'MakeAbsolutePath',
-'MakeRelativePath', 
-'RemoveAncestorPaths', 
-'RemoveCommonSuffix',
-'RemoveDescendentPaths', 
-'ReplaceName',
-'ReplacePrefix', 
-'ReplaceTargetPath', 
-'StripAllVariantSelections',
-'StripNamespace', 
-'StripPrefixNamespace',
-
-
-'expressionIndicator',
-
-'mapperArgDelimiter',
-'mapperIndicator',
-
-'reflexiveRelativePath',
-'relationshipTargetEnd', 
-'relationshipTargetStart', 
-'targetPath']
+# When authoring relationships, we usually want to remove all variant encodings in the path:
+variant_path = Sdf.Path('/set/bicycle{style=blue}frame/screws')
+prim_rel_target_path = variant_path.StripAllVariantSelections() # Returns: Sdf.Path('/set/bicycle/frame/screws')
+#// ANCHOR_END: pathVariants
--- docs/src/core/elements/path.md
+++ docs/src/core/elements/path.md
@@ -1,5 +1,4 @@
 # Paths
-
 As Usd is a hierarchy based format, one of its core functions is handling paths.
 In order to do this, Usd provides the pxr.Sdf.Path class. You'll be using quite a bunch, so that's why we want to familiarize ourselves with it first.
 
@@ -12,25 +11,60 @@ pxr.Sdf.Path("/My/Example/Path")
 ## Resources
 - [API Docs](https://openusd.org/release/api/class_sdf_path.html#sec_SdfPath_Overview)
 
-
+## TL;DR - Paths In-A-Nutshell
+Here is the TL;DR version:
+- Paths can encode the following path data:
+    - Prims ("/set/bicycle")
+    - Properties:
+        - Attributes: "." ("/set/bicycle.size")
+        - Relationships: "." ("/set.bikes")
+            - Prim to prim: ("/set.bikes[/set/bicycles]") (E.g. Collections of primpaths)
+            - Attribute to attribute: ("/set.size[/set/bicycles].size") (E.g. Serializing node graph connections)
+    - Variants ("/set/bicycle{style=blue}wheel.size")
+    
+~~~admonish tips title=""
+```python
+{{#include ../../../../code/core/elements.py:pathSummary}}
+```
+~~~
 
 ## Basics
-
 Each element in the path between the "/" symbol is a [prim](https://openusd.org/release/glossary.html#usdglossary-prim) similar to how on disk file paths mark a folder or a file.
-~~~admonish tips title="Creating a path & string representation"
+
+We recommend going through these small examples (5-10 min), just to get used to the Path class.
+
+### Creating a path & string representation
+
+~~~admonish tips title=""
 ```python
 {{#include ../../../../code/core/elements.py:pathBasics}}
 ```
 ~~~
 
-~~~admonish tips title="Special Paths: emptyPath & absoluteRootPath"
+### Special Paths: emptyPath & absoluteRootPath
+
+~~~admonish tips title=""
 ```python
 {{#include ../../../../code/core/elements.py:pathSpecialPaths}}
 ```
 ~~~
 
-~~~admonish tips title="Encoding properties (attributes/relationships) in the path"
+### Variants
+We can also encode variants into the path via the {variantSetName=variantName} syntax.
+
+~~~admonish tips title=""
+```python
+{{#include ../../../../code/core/elements.py:pathVariants}}
+```
+~~~
+
+### Properties
+Paths can also encode properties (more about what these are in the next section).
+Notice that attributes and relationships are both encoded with the "." prefix, hence the name "property" is used to describe them both.
+
+~~~admonish tips title=""
 ```python
 {{#include ../../../../code/core/elements.py:pathProperties}}
 ```
 ~~~
+
