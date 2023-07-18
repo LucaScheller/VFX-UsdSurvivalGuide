875438a375322201e5a149e290ae5757757c6b98
Luca Scheller <luca.scheller@gmail.com>
2023-07-18 15:30:48 +0200

    Add intro to compiling

--- files/plugins/schemas/compiledSchema/build.sh
+++ files/plugins/schemas/compiledSchema/build.sh
@@ -8,14 +8,17 @@ rm -R src
 rm -R dist
 # Invoke usdGenSchema
 usdGenSchema schema.usda ./src
+# Copy auxiliary files
+cp -r auxiliary/* src
+# Build
+rm -R build
+rm -R dist
+cmake . -B build
+cmake --build build --clean-first              # make clean all
+cmake --install build                          # make install
 # Set plugin path
 current_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && (pwd -W 2> /dev/null || pwd))
 if [[ "$PXR_PLUGINPATH_NAME" != *"$current_dir/dist"* ]]; then
   export PXR_PLUGINPATH_NAME=$current_dir/dist:${PXR_PLUGINPATH_NAME}
   echo "Added $current_dir/dist to the PXR_PLUGINPATH_NAME environment variable."
 fi
-#rm -R build
-#rm -R dist
-#cmake . -B build
-#cmake --build build --clean-first              # make clean all
-#cmake --install build                          # make install
