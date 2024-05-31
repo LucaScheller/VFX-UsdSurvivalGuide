5a5c55130af3f2df9db41049f8431d7c8de90030
Markus Raudaschl <4717157+mrowdy@users.noreply.github.com>
2024-05-31 14:56:14 +0200

    Fix typos

--- code/core/elements.py
+++ code/core/elements.py
@@ -475,7 +475,7 @@ from pxr import Sdf, Tf, Usd, UsdGeom
 stage = Usd.Stage.CreateInMemory()
 prim_path = Sdf.Path("/bicycle")
 prim = stage.DefinePrim(prim_path, "Xform")
-prim.ApplyAPI("UsdGeomModelAPI")
+prim.ApplyAPI("GeomModelAPI") # Older USD versions: prim.ApplyAPI("UsdGeomModelAPI")
 prim_def = prim.GetPrimDefinition()
 print(prim_def.GetAppliedAPISchemas()) # Returns: ['GeomModelAPI']
 print(prim_def.GetPropertyNames()) 
@@ -533,7 +533,7 @@ purpose_attr.Set(UsdGeom.Tokens.render)
 ## Payload loading: Control payload loading (High Level only as it redirects the request to the stage).
 # In our example stage here, we have no payloads, so we don't see a difference.
 prim.Load()
-prim.UnLoad()
+prim.Unload()
 # Calling this on the prim is the same thing.
 prim = stage.GetPrimAtPath(prim_path)
 prim.GetStage().Load(prim_path)
@@ -2474,7 +2474,7 @@ attr.FlattenTo(prim, "someNewName")
 # Note the method signature takes a time code as an input. If you supply a default time code
 # value clips will be stripped from the result.
 time_code = Usd.TimeCode(1001)
-print(prim.GetPropertyStack(time_code))
+print(attr.GetPropertyStack(time_code))
 ### Low Level ###
 # The low level API does not offer any "extras" worthy of noting.
 #// ANCHOR_END: propertyOverview
